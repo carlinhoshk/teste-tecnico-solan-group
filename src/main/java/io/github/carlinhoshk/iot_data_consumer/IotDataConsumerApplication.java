5aab7707c25c070929474a32b83db0bc8e98d108
Carlos Oliveira <carlosmdohk@gmail.com>
2026-06-14 03:25:45 -0300

    feat: adicionar arquivos iniciais para projeto

---
+++ src/main/java/io/github/carlinhoshk/iot_data_consumer/IotDataConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.github.carlinhoshk.iot_data_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotDataConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotDataConsumerApplication.class, args);
+	}
+
+}
