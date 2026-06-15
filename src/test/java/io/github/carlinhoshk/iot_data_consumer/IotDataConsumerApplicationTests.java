2db8ad71cce5bea3a16096648a3493f97b95b367
Carlos Oliveira <carlosmdohk@gmail.com>
2026-06-15 18:01:27 -0300

    feat: adicionar scripts do Maven Wrapper

---
+++ src/test/java/io/github/carlinhoshk/iot_data_consumer/IotDataConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.carlinhoshk.iot_data_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotDataConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
